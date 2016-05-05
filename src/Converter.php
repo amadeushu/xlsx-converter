b5a7070a795118d6af364f20b86118ec2e84ae09
Tamas Balint <tbalint@hu.amadeus.com>
2016-05-05 16:16:56 +0200

    oszlop elnevezesek hiba javitasa

--- src/Converter.php
+++ src/Converter.php
@@ -35,37 +35,6 @@ class Converter
   private $array;
 
   /**
-   * Excel oszlopnevek tömbjének visszaadása
-   *
-   * @param   string  $end_column  Az oszlopok utolsó tagja
-   * @return  array                Az oszlopneveket tartalmazó tömb
-   */
-  private function createColumnsArray($end_column)
-  {
-    $columns = array();
-    $length = strlen($end_column);
-    $letters = range('A', 'Z');
-    foreach ($letters as $letter)
-    {
-      $column = $letter;
-      $columns[] = $column;
-      if ($column == $end_column)
-      {
-        return $columns;
-      }
-    }
-    foreach ($columns as $column)
-    {
-      if (!in_array($end_column, $columns) && strlen($column) < $length)
-      {
-        $new_columns = $this->createColumnsArray($end_column, $column);
-        $columns = array_merge($columns, $new_columns);
-      }
-    }
-    return $columns;
-  }
-
-  /**
    * Konstruktor
    *
    * @param  array  $array  A feldolgozandó adatok tömbje
@@ -73,10 +42,78 @@ class Converter
   public function __construct($array)
   {
     // Felépítjük az excel oszlopnevek tömbjet
-    $this->columns = $this->createColumnsArray('BB');
+    $columns = function($column_value)
+    {
+      // szamlalo
+      $count = 26;
+
+      // ideigelenes tombok
+      $array = $in_array = $response = array();
+
+      // karakterek szama
+      $char_number  = ceil($column_value / $count);
+
+      // elotag karakterek ... ha az oszlopok szama meghaladja az A - Z intervallumot, akkor innet veszi az ertekeket, elotagokat
+      $prefix_array = range(chr(65), chr(64 + ($char_number)));
+
+      // letrehozzuk az ideiglenes tombot
+      for ($i = 0; $i < $char_number; $i++)
+      {
+        // karakterek generalasa
+        $array[] = range(chr(65), chr(64 + ($column_value > $count ? $count : $column_value)));
+
+        // csokkentjuk a sorok szamat
+        $column_value = $column_value - $count;
+      }
+
+      // bejarjuk, egyesitjuk a tombot
+      foreach ($array as $sub_array)
+      {
+        foreach ($sub_array as $value)
+        {
+          $in_array[] = $value;
+        }
+      }
+
+      // bejarjuk, rendezzuk a tombot
+      foreach ($in_array as $value)
+      {
+        // ha meg nincs benne az adott karakter a tombben, akkor beletesszuk
+        if (!in_array($value, $response))
+        {
+          $response[] = $value;
+        }
+        // de ha mar benne van, akkor
+        else
+        {
+          // ha a szamlalo 26, akkor csak siman kivesszuk a prefix, elotag tombbol az elso elemet
+          if ($count == 26)
+          {
+            $prefix  = array_shift($prefix_array);
+          }
+          // ha a szamlalo 0, akkor ujra kezdjuk a szamlalast, es kivesszuk a prefixet
+          elseif ($count == 0)
+          {
+            $count  = 26;
+            $prefix = array_shift($prefix_array);
+          }
+
+          // hozzaadjuk a tombhoz az oszlop nevet
+          $response[] = $prefix.$value;
+
+          // csokkentjuk a szamlalo erteket
+          $count--;
+        }
+      }
+
+      // visszaterunk az oszlopok neveivel
+      return $response;
+    };
+
+    $this->columns = $columns(count($array[0]));
 
     // Osztályváltozóba tesszük a kapott tömböt
-    $this->array = $array;
+    $this->array   = $array;
 
     // Az excel kulccsal ellátott tömb létrehozása
     $this->build();
